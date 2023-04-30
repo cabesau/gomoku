b4cf4e0013b1d360003bfbaac3f170416d4074a4
椎橋涼太 <ryota@mac.local>
2023-04-30 22:17:42 +0900

    非同期処理開始

--- resources/views/wait_room.blade.php
+++ resources/views/wait_room.blade.php
@@ -27,7 +27,7 @@
         @if ($room_maker)
         <form action="battle" method="post">
         @csrf
-
+            <input type="hidden" name="room" value=$room>
             <div class="flex justify-center">
                 <button type="submit" class="mb-8 py-3 px-4 mt-10 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-gray-500 text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition-all text-sm dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-offset-gray-800"id="createRoomBtn">
                     ゲームを開始する
