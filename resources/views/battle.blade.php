653d77805086b2570ed9fad405c6f404ce968c6b
椎橋涼太 <ryota@mac.local>
2023-06-01 09:43:24 +0900

    battleの処理をフロント寄りにした。topからurlを使わないようにする

--- resources/views/battle.blade.php
+++ resources/views/battle.blade.php
@@ -1,14 +1,13 @@
 <x-layout>
     <x-menu></x-menu>
-    
-{{$json_data['player1_name']}}VS{{$json_data['player2_name']}}<br>
+<div>
+    <span id="player1_name"></span>
+    <span>VS</span>
+    <span id="player2_name"></span>
+</div>    
 <div class="flex justify-center">
     <div class="flex flex-col">
-        <div id="turn_player" class="cursor-pointer rounded border bg-yellow-500 pb-10 tex-center">
-            @if ($json_data['win_player'])
-            {{$json_data['win_player']}}の勝利
-            @endif
-        </div>
+        <div id="turn_player" class="cursor-pointer rounded border bg-yellow-500 pb-10 tex-center"></div>
         <table class="border-collapse border border-slate-900">
             
             @for($i=1; $i<=14; $i++)
@@ -16,21 +15,8 @@
                 @for($j=1; $j<=14; $j++)
                 <?php $squ_num = $j + 14 * ($i - 1); ?>
                 <td class="border border-slate-900 w-8 h-8 text-center bg-amber-100 " >
-                    @if (($json_data["squ_{$squ_num}"] == 0))
-                    @if (!$json_data['win_player'])
-                    <div  class="justify-center">
-                        <a href={{ route('battle', ['room_no'=>$json_data['room_no'],'room_maker'=>$room_maker,'squ_num'=>$squ_num]) }}>
-                            ・
-                        </a> 
-                        @else
-                        .
-                    </div>
-                    @endif
-                    @elseif($json_data["squ_{$squ_num}"] == 1)
-                    ◯
-                    @elseif($json_data["squ_{$squ_num}"] == 2)
-                    ●
-                    @endif
+
+                <a id={{$squ_num}} href={{ route('battle', ['room_no'=>$json_data['room_no'],'room_maker'=>$room_maker,'squ_num'=>$squ_num]) }}></a> 
                     
                 </td>
                 @endfor  
@@ -47,67 +33,109 @@
     console.log(`ルームナンバーは${room_no}`);
 
     let room_maker = {{$room_maker}};
-    console.log(room_maker);
+    console.log('ルームメイカー：'+room_maker);
+
+    let win_player = {{$json_data['win_player']}};
+
+    //初回jsonファイル取得
+    check_json(room_no);
 
     //ajax通信でjsonファイルを取得
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-
-    $.ajax({
-    url: '/return_json',
-    type: 'POST',
-    dataType: 'json',
-    data:{room_no: room_no},
-    })
-    .done( function(response) {
-        // 取得したJSONデータを表示
-        console.log('取得できました');
-        
-        // 手番の制御
-        let data = response[0];
-        console.log(data.game_counter);
-        console.log(data);
-        
-        turn_player(data.game_counter);
-        // location.reload();
-
-    })
-    .fail( function(jqXHR, textStatus, errorThrown) {
-        // console.error(error);
-        console.log('失敗');
-        console.log("ajax通信に失敗しました");
-        console.log("jqXHR          : " + jqXHR.status); // HTTPステータスが取得
-        console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラー
-        console.log("errorThrown    : " + errorThrown); // 例外情報
-        console.log("URL            : " + '/return_json');
-    });
-
-    function turn_player(game_counter){
-        if(room_maker){
-            if(game_counter % 2 == 1){
-                $("#turn_player").text("あなたの番です");
-                $('a').off('click'); // aタグのクリックイベントの処理を解除して有効にする
-            }else{
-                $("#turn_player").text("お待ちください");
-                $('a').click(function(event) {
-                    event.preventDefault(); 
-                });
+    
+    function check_json(room_no){
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
+        });
+        $.ajax({
+            url: '/return_json',
+            type: 'POST',
+            dataType: 'json',
+            data:{room_no: room_no},
+        })
+        .done( function(json_data) {
+            // 取得したJSONデータを表示
+            console.log('取得できました');
+            let data = json_data[0];
+            console.log('game_counter:'+data.game_counter);
+            
+            //プレイヤー名を表示
+            display_player(data);
+            
+            //プレイヤーの名前を表示
+            display_turn_player(data.game_counter);
+            
+            //盤面の描画
+            decide_color(data);
+            let counter = 'game_counter';
+            console.log(data[`${counter}`]);
+
+            //1秒後にポーリング
+            setTimeout(function(){
+                check_json(room_no);
+            } ,5000,room_no);
+            
+        })
+        .fail( function(jqXHR, textStatus, errorThrown) {
+            // console.error(error);
+            console.log('失敗');
+            console.log("ajax通信に失敗しました");
+            console.log("jqXHR          : " + jqXHR.status); // HTTPステータスが取得
+            console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラー
+            console.log("errorThrown    : " + errorThrown); // 例外情報
+            console.log("URL            : " + '/return_json');
+        });
+    }
+    
+    //プレイヤーの名前を表示
+    function display_player(data){
+        $("#player1_name").text(data['player1_name']);
+        $("#player2_name").text(data['player2_name']);
+        console.log(data['player1_name']);
+
+    }
+
+    //盤面に白か黒の石を置くメソッド
+    function decide_color(data){
+        for(let i = 0; i < 195; i++){
+            if(data[`squ_${i}`] == 0){
+                $(`#${i}`).text('・');
+            }else if(data[`squ_${i}`] == 1){
+                $(`#${i}`).text('●');
+            }else{
+                $(`#${i}`).text('◯');
+            }
+        }
+    }
+
+    //勝利プレイヤーか手番プレイヤーを表示
+    function display_turn_player(game_counter){
+        if(win_player != 0){
+            $("#turn_player").text(`${win_player}さんの勝利です`);
         }else{
-            if(game_counter % 2 == 0){
-                $("#turn_player").text("あなたの番です");
-                $('a').off('click'); // aタグのクリックイベントの処理を解除して有効にする
+            if(room_maker){
+                if(game_counter % 2 == 1){
+                    $("#turn_player").text("あなたの番です");
+                    $('a').off('click'); // aタグのクリックイベントの処理を解除して有効にする
+                }else{
+                    $("#turn_player").text("お待ちください");
+                    $('a').click(function(event) {
+                        event.preventDefault(); 
+                    });
+                }
             }else{
-                $("#turn_player").text("お待ちください");
-                $('a').click(function(event) {
-                    event.preventDefault();
-                });
+                if(game_counter % 2 == 0){
+                    $("#turn_player").text("あなたの番です");
+                    $('a').off('click'); // aタグのクリックイベントの処理を解除して有効にする
+                }else{
+                    $("#turn_player").text("お待ちください");
+                    $('a').click(function(event) {
+                        event.preventDefault();
+                    });
+                }
             }
         }
-
     }
 
     
